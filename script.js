c14a265009c2e495329cca829bf281ebf6ed1c68
zizadevv <158997219+zizadevv@users.noreply.github.com>
2024-03-05 22:02:08 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+// console.log(1);
+
+
+
+
+// var names = 'Иван'
+// var year = 1990
+// var boolean =true
+// var und =false
+
+// console.log(names);
+// console.log(year);
+// console.log(boolean);
+// console.log(und);
+
+
+
+
+// var allinfo = 'Я '+ names + ' мне '+ year + ' лет!'
+// console.log(allinfo)
+
+
+
+
+// console.log('8/4=' + (8/4))
+
+
+
+
+var str = prompt('Введите свое имя')
+console.log(str)
+
+
+
+
+
+var str = prompt('Введите свое год рождения')
+console.log(str)
+
+
+
+
+var str = prompt('Решите пример 5 + 5 = ?')
+console.log('5 + 5 =' + (5 + 5));
+
+
+
+
+
+var str = prompt('Решите пример 5 * 5 = ?')
+console.log('5 * 5 =' + (5 * 5));
+
+
+
+var str = prompt('Решите пример 18 - 11 = ?')
+console.log('18 - 11 =' + (18 - 11));
+
+
+
+var str = prompt('Решите пример 18 / 6 = ?')
+console.log('18 / 6 =' + (18 / 6));
+
+
+
+var str = prompt('Решите пример 11 % 2 = ?')
+console.log('11 % 2 =' + (11 % 2));
+
+
+
+
